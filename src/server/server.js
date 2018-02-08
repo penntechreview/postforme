11bc3ab0f8abf4d5d23d34187e4e382ccb83ed5f
Nihar Patil <nihar.patil98@gmail.com>
2018-02-08 14:44:55 -0500

    stuff

--- src/server/server.js
+++ src/server/server.js
@@ -33,41 +33,11 @@ app.use('/', schedulerouter(DB))
 const schedule = require('node-schedule')
 const request = require('request-promise');
 
-
-// FOR TESTING PURPOSES
-
-const postTextOptions = {
-  method: 'POST',
-  uri: `https://graph.facebook.com/v2.11/me/feed`,
-  qs: {
-    access_token: "EAADB7HZCrmRkBANKrkl4VKCW2OKb1NR66OKGO8kZB5aM4DIuZAOiX9PA67VS7Kl6dY82fgGESkSC0ZAZA9Hzf82ejVzH989Xfmvx3Pc3VA4g7RcXjwjuO85rnIG5ZAdQpYUyXRLTTtHJxdZClkFAXr65zWrLH3zadOIFj52Up0qZCQZDZD",
-    message: 'Hello world'
-  }
-};
-request(postTextOptions)
-.then(fbRes => {
-  const fbpostid = JSON.parse(fbRes).id
-  DB.postedPost(fbpostid, created.id)
-  .then(post => {
-    console.log(post)
-  })
-})
-.then()
-.catch(error => {
-  console.log(error)
-})
-
-// END TESTS
-
-
-
 DB.getAllPosts()
 .then(posts => {
   posts.forEach(post => {
     var dt = new Date(post.posttime)
-    if (process.env.PRODUCTION) {
-      dt.setTime(dt.getTime() + (5*60*60*1000))
-    }
+    console.log(post.postcontent.substring(0,10)," ", dt)
     const job = schedule.scheduleJob(dt, function(){
       //Post the content to facebook, then change the status of whether content has successfully posted
       const postTextOptions = {
