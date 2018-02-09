35ae2cc6228fc8b67da83482f13a46bf8228b76c
Nihar Patil <nihar.patil98@gmail.com>
2018-02-08 19:22:35 -0500

    poopy

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -54,6 +54,16 @@ function postedPost(fbpostid, postid) {
   })
 }
 
+Post.findById("5a7aac2405f70a312c7beb77")
+.then(post => {
+  var dt = new Date(post.posttime)
+  post.posttime = new Date(dt.setTime(dt.getTime() + 60*1000))
+  return post.save()
+})
+.then(post => {
+  console.log(post)
+})
+
 module.exports = {
   findOrCreateUser,
   findUserById,
